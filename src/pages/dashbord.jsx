7078a356854ec52795d8cb86c5d71a4d2abd3c10
Ameer Muhammad <amyrmian789@gmail.com>
2026-06-12 16:16:38 +0500

    timer created

--- src/pages/dashbord.jsx
+++ src/pages/dashbord.jsx
@@ -5,47 +5,57 @@ import { useState, useEffect } from "react";
 
 const DashBoard = () => {
     const [timer, setTimer] = useState(1500)
+    const [running, isRunning] = useState(false)
 
-    let timeLeft = 1500 
-    const minutes = Math.floor(timeLeft / 60); 
-    const seconds = minutes % 60;
 
     useEffect(() => {
+        if (!isRunning) return;
 
-       setTimeout(setTimer(timeLeft = timeLeft-1, 1000) )
+        const interval = setInterval(() => {
+            setTimer((prev) => {
+                if (prev <= 1) {
+                    clearInterval(interval)
+                    return 0;
+                }
+                return prev - 1
+            })
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [isRunning])
+
+    const minutes = Math.floor(timer / 60);
+    const seconds = timer % 60;
 
 
 
-      
 
-        return (
 
-            <>
-                <section className="bg-[url(src/assets/images/bg.jpg)] bg-cover 
+    return (
+
+        <>
+            <section className="bg-[url(src/assets/images/bg.jpg)] bg-cover 
                                 h-screen w-full bg-no-repeat bg-center
                                 font-inter text-[#E8F3FC] relative p-4 sm:p-6 md:p-10 box-border overflow-x-hidden" >
 
-                    <div className="bg-gray-500/20 backdrop-blur-xl rounded-2xl border border-white/20 shadow-lg backdrop-saturate-150 
+                <div className="bg-gray-500/20 backdrop-blur-xl rounded-2xl border border-white/20 shadow-lg backdrop-saturate-150 
                                grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6
                                auto-rows-max p-4 sm:p-6 md:p-8 w-full box-border">
-                        <div className="sm:col-span-1 lg:col-span-2">
-                            <Image file={"src/assets/images/soundsImages/birds.png"} />
-                        </div>
-                        <div className="sm:col-span-1 row-span-2 ">
-                            <History />
-                        </div>
-                        <div className="sm:col-span-2 lg:col-span-2" >
-                            <Timer pomodoro={timer} />
-                        </div>
-
-
+                    <div className="sm:col-span-1 lg:col-span-2">
+                        <Image file={"src/assets/images/soundsImages/birds.png"} />
                     </div>
+                    <div className="sm:col-span-1 row-span-2 ">
+                        <History />
+                    </div>
+                    <div className="sm:col-span-2 lg:col-span-2" >
+                        <Timer pomodoro={`${minutes}:${seconds}`} />
+                    </div>
+                </div>
 
-                </section>
+            </section>
 
-            </>
-        )
-    })
+        </>
+    )
+
 
 }
 
